da5ffe010ea4472eff36a9bdc3e6365e773efcc8
uenobu106 <uenobu106@ezweb.ne.jp>
2017-10-19 12:43:42 +0900

    編集機能とリファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,14 @@
 class PostsController < ApplicationController
 
+  before_action :set_post, only:[ :show, :edit, :update ]
+
   def new
     @post = Post.new
   end
 
   def create
     @post = Post.new(params_post)
-    if @post.save!
+    if @post.save
       redirect_to '/posts'
     else
       render action: 'new'
@@ -18,7 +20,17 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(params_post)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
   end
 
 
@@ -26,6 +38,10 @@ class PostsController < ApplicationController
 
   def params_post
     params.require(:post).permit(:title, :content, :source)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
