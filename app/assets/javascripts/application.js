050204f237021c136f15ff964ea239b8599b713b
uenobu106 <uenobu106@ezweb.ne.jp>
2017-11-12 19:24:04 +0900

    フロント修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,12 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery_ujs
 //= require turbolinks
 //= require jquery
 //= require bootstrap
 //= require material
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
